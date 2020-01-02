8cfdecf837cd5701e03d106b104ffbd64e991c3e
Ali AlNaghmoush <alialnaghmoush@gmail.com>
2020-01-02 19:04:45 +0300

    Initial Commit

---
+++ SwiftUI FullModal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI FullModal
+//
+//  Created by Ali AlNaghmoush on 02/01/2020.
+//  Copyright © 2020 Ali AlNaghmoush. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
